5513a61866966fdde6e7bcee0e1cf63eebe107d3
GaoJunJie <594402100@qq.com>
2018-04-26 10:43:12 +0800

    be a better man

--- demo/ejs/index.js
+++ demo/ejs/index.js
@@ -1,20 +1,23 @@
-const Koa = require('koa')
-const views = require('koa-views')
-const path = require('path')
-const app = new Koa()
+const Koa = require('koa');
+const views = require('koa-views');
+const path = require('path');
+const app = new Koa();
 
+//加载模板引擎
 app.use(views(path.join(__dirname, './view'), {
   extension: 'ejs'
-}))
+}));
 
 app.use( async ( ctx ) => {
-  let title = 'hello koa2'
+  let title = 'hello koa2';
+  let name = 'Jonathan';
   await ctx.render('index', {
     title,
-  })
-})
+    name,
+  });
+});
 
 app.listen(3000, ()=>{
   console.log('[demo] ejs is starting at port 3000')
-})
+});
 
--- demo/session/index.js
+++ demo/session/index.js
@@ -1,33 +1,38 @@
-const Koa = require('koa')
-const session = require('koa-session-minimal')
-const MysqlSession = require('koa-mysql-session')
+const Koa = require('koa');
+const session = require('koa-session-minimal');
+const MysqlSession = require('koa-mysql-session');
 
-const app = new Koa()
+const app = new Koa();
 
 // 配置存储session信息的mysql
 let store = new MysqlSession({
   user: 'root',
-  password: 'abc123',
+  password: 'root',
   database: 'koa_demo',
   host: '127.0.0.1',
-})
+});
 
 // 存放sessionId的cookie配置
 let cookie = {
-  maxAge: 10 * 60 * 1000, // cookie有效时长
-  expires: new Date('2017-02-15'),  // cookie失效时间
-  path: '/index', // 写cookie所在的路径
-  domain: 'localhost', // 写cookie所在的域名
-  httpOnly: false, // 是否只用于http请求中获取
-  overwrite: false,  // 是否允许重
-}
+  maxAge: '', // cookie有效时长
+  expires: '',  // cookie失效时间
+  path: '', // 写cookie所在的路径
+  domain: '', // 写cookie所在的域名
+  httpOnly: '', // 是否只用于http请求中获取
+  overwrite: '',  // 是否允许重写
+  secure: '',
+  sameSite: '',
+  signed: '',
+  
+};
+
 
 // 使用session中间件
 app.use(session({
   key: 'SESSION_ID',
   store: store,
   cookie: cookie
-}))
+}));
 
 app.use( async ( ctx ) => {
 
@@ -45,9 +50,9 @@ app.use( async ( ctx ) => {
     ctx.body = ctx.session
   } 
   
-})
+});
 
 app.listen(3000, () => {
   console.log('[demo] session is starting at port 3000')
-})
+});
 
